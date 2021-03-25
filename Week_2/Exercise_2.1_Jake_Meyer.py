3b289aa194b74334b0a59b73202c24dd029028e6
jkmeyer1010 <70782393+jkmeyer1010@users.noreply.github.com>
2021-03-25 08:24:24 -0600

    Added Exercise 2.1
    Added .py and .pdf files to Week 2 Folder.

---
+++ Week_2/Exercise_2.1_Jake_Meyer.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Jake Meyer
+Assignment 2.1: Preparing for EDA
+Date: 03/25/2021
+"""
+# Display the text “Hello World! 
+# I wonder why that is always the default coding text to start with” 
+print("Hello World! I wonder why that is always "
+      "the default coding text to start with")
+
+# Add two numbers together
+x = 2
+y = 3
+z = x + y
+print(x,"+",y,"=",z)
+
+# Subtract a number from another number
+a = 10
+b = 20
+c = b - a
+print(b,"-",a,"=",c)
+
+# Multiply two numbers
+i  = x * y
+print(x,"*",y,"=",i)
+
+# Divide between two numbers
+k = b // a
+print(b,"/",a,"=",k)
+
+# Concatenate two strings together (any words)
+first_name = "John "
+last_name = "Doe"
+name = first_name + last_name
+print(name)
+
+# Create a list of 4 items (can be strings, numbers, both)
+list = [1, "John", 8, "a"]
+print(list)
+
+# Append an item to your list (again, can be a string, number)
+list.append(9)
+print(list)
+
+# Create a tuple with 4 items (can be strings, numbers, both)
+t = (1, "a", 2, "b")
+print(t)
